03693596541272a08d15bcac713db378930a2410
trenton <colemantrenton@gmail.com>
2015-04-08 16:55:28 -0700

    test

---
+++ src/teamfatal/TableItem.java
@@ -0,0 +1,38 @@
+package teamfatal;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+/**
+ * Created by trenton on 4/2/15.
+ */
+public class TableItem extends JLabel
+{
+    int id;
+    Receipt tableReceipt;
+
+    public TableItem()
+    {
+        super();
+        ImageIcon icon = createImageIcon("Resources/Images/table.png", "");//
+        this.setIcon(icon);
+        this.setVisible(true);
+        tableReceipt = new Receipt();
+    }
+
+    public Receipt getReceipt() {
+        return tableReceipt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    private ImageIcon createImageIcon(String path, String description)
+    {
+        return new ImageIcon(path, description);
+    }
+}

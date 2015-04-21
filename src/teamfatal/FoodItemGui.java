092dd263ab4c07b4bd9766aab0fc399f5b3e5edb
trenton <colemantrenton@gmail.com>
2015-04-20 17:06:10 -0700

    More Stuff

--- src/teamfatal/FoodItemGui.java
+++ src/teamfatal/FoodItemGui.java
@@ -7,28 +7,49 @@ import java.awt.*;
  * Created by trenton on 4/10/15.
  */
 public class FoodItemGui extends JLabel {
-
-    private Image image;
     private FoodItem myFood;
 
+    /**
+     * Constructor
+     */
     public FoodItemGui() {
         super();
     }
 
-    public Image getImage() {
-        return image;
+    /**
+     * Sets the ImageIcon of the FoodItemGui
+     * @param path Path to the image file to be used
+     */
+    public void setImage(String path)
+    {
+        ImageIcon icon = createImageIcon(path, "");
+        this.setIcon(icon);
+        this.setVisible(true);
     }
 
-    public void setImage(Image image) {
-        this.image = image;
-    }
-
+    /**
+     * Gets FoodItem
+     * @return FoodItem
+     */
     public FoodItem getMyFood() {
         return myFood;
     }
 
+    /**
+     * @param myFood FoodItem to set FoodItemGui to
+     */
     public void setMyFood(FoodItem myFood) {
         this.myFood = myFood;
     }
 
+    /**
+     * Create and return ImageIcon of the image
+     * @param path Image location for the icon
+     * @param description Description of the image
+     * @return ImageIcon of the image
+     */
+    protected ImageIcon createImageIcon(String path, String description)
+    {
+        return new ImageIcon(path, description);
+    }
 }

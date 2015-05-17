cd112fe714b9485e4fc460737b7e21d90a73882e
AlanD88 <alanduncan3@gmail.com>
2015-05-17 15:00:22 -0700

    Adjusted images and added new ones

--- src/teamfatal/ToGoOrder.java
+++ src/teamfatal/ToGoOrder.java
@@ -29,7 +29,7 @@ public class ToGoOrder extends OrderObject {
     {
         super();
         this.name = name;
-        ImageIcon icon = createImageIcon("Resources/Images/Togo.jpg", "");
+        ImageIcon icon = createImageIcon("Resources/Images/togobox.png", "");
         this.setIcon(icon);
         this.setVisible(true);
         paid = false;
